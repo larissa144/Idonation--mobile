0053268cd78810ca7296af9929e65119dd51f5ed
Larissa Alves d Silva <larissa.dasilva@grupofleury.com.br>
2020-04-26 18:08:28 -0300

    Tela de inicio e cadastro inicial

---
+++ Idonation/app/src/main/java/com/example/idonation/cad_doadro_pass1.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cad_doadro_pass1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad_doadro_pass1)
+    }
+}
