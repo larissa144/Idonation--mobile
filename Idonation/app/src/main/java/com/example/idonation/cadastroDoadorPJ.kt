f759854cf6442fc6cbf1be03760a09aaf89d07c1
michelle.dmessias <michelle.dmessias@grupofleury.com.br>
2020-04-27 18:06:46 -0500

    Formulario de cadastro doadores

---
+++ Idonation/app/src/main/java/com/example/idonation/cadastroDoadorPJ.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cadastroDoadorPJ : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_doador_p_j)
+    }
+}
