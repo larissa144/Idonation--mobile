ba9efa50ad885249bc0af23184379a5398d92367
giovannasantos <giovanna.santos@bandtec.com.br>
2020-04-28 16:17:10 -0300

    Tela de cadastro das Ongs

---
+++ Idonation/app/src/main/java/com/example/idonation/CadOngPartUm.kt
@@ -0,0 +1,13 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadOngPartUm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad_ong_part_um)
+
+    }
+}
