87eb29fb3b62eb5ace16646add6628c13e825938
cristielen <cristielenoliveiradebarros@gmail.com>
2020-04-28 08:35:04 -0300

    telas: login - cnpj/cpf/ong

---
+++ Idonation/app/src/main/java/com/example/idonation/login_cnpj.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_cnpj : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_cnpj)
+    }
+}
