b573f2d8cd97ebf13e0b3724cd6a502544ac3cf3
michelle.dmessias <michelle.dmessias@grupofleury.com.br>
2020-06-10 15:14:26 -0500

    Telas Michelle, Ong buttons e publicações

---
+++ Idonation/app/src/main/java/com/example/idonation/Home1Ongs.kt
@@ -0,0 +1,11 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home1Ongs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home1_ongs)
+    }
+}
---
+++ Idonation/app/src/main/java/com/example/idonation/publicacoesMatchOng.kt
@@ -0,0 +1,11 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class publicacoesMatchOng : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publicacoes_match_ong)
+    }
+}
