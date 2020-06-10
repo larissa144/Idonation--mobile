b6e4a225a9c6e412a06445676f88dc948f2903a0
giovanna <giovannalves04@gmail.com>
2020-06-10 18:58:42 -0300

    Tela de publicacoes e home de doadores

---
+++ Idonation/app/src/main/java/com/example/idonation/Outros.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Outros : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outros)
+    }
+}
---
+++ Idonation/app/src/main/java/com/example/idonation/home1_doador.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home1_doador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home1_doador)
+    }
+}
