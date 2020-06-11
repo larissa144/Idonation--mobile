a5c84590e84cb65e32b2f68e7f3df1e6dbce0f26
Cristielen.barros <cristielen.barros@accesstage.com.br>
2020-06-11 13:56:13 -0300

    refatorando as telas (doacoes - mensagens - match)

---
+++ Idonation/app/src/main/java/com/example/idonation/doacoes_de_interesse.kt
@@ -0,0 +1,11 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class doacoes_de_interesse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doacoes_de_interesse)
+    }
+}
