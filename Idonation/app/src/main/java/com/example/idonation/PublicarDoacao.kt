074909ceb9df5625f20e59087ade9c02a03fba89
Larissa Alves d Silva <larissa.dasilva@grupofleury.com.br>
2020-06-23 12:03:26 -0300

    Telas: publicar e fazer doação

---
+++ Idonation/app/src/main/java/com/example/idonation/PublicarDoacao.kt
@@ -0,0 +1,12 @@
+package com.example.idonation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicarDoacao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publicar_doacao)
+    }
+}
