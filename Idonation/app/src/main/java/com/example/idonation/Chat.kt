95f02a7cde2f433eec170cfcf20d8f81e77d2d72
giovanna <giovanna.santos@bandtec.com.br>
2020-07-02 00:27:33 -0300

    chat

---
+++ Idonation/app/src/main/java/com/example/idonation/Chat.kt
@@ -0,0 +1,46 @@
+package com.example.idonation
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_chat.*
+
+class Chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+        if(webView != null){
+            val webSettings = webView!!.settings
+            webSettings.javaScriptEnabled = true
+
+            webView!!.webViewClient = WebViewClient()
+            webView!!.webChromeClient = WebChromeClient()
+            webView!!.loadUrl("https://app.gotomeeting.com/index.html?meetingId=285952677")
+
+            webView!!.webViewClient = object: WebViewClient(){
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    progressBar.visibility = View.VISIBLE
+                    super.onPageStarted(view, url, favicon)
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    progressBar.visibility = View.GONE
+                    super.onPageFinished(view, url)
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()){
+            webView!!.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+}
--- Idonation/app/src/main/java/com/example/idonation/match.kt
+++ Idonation/app/src/main/java/com/example/idonation/match.kt
@@ -1,11 +1,21 @@
 package com.example.idonation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class match : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
+    }
+
+    fun irPChat(v: View){
+
+        val irPChat = Intent(this, Chat::class.java)
+
+        startActivity(irPChat)
+
     }
 }
